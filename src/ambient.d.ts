8f1912a35de933cd7366b2e9dc6eb85f5ce40714
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-04-16 19:11:23 +0200

    Add global types file

---
+++ src/ambient.d.ts
@@ -0,0 +1,13 @@
+declare type UserInfo = {
+    username: String,
+    gender: String,
+    age: Number,
+    country: String
+}
+
+declare type TodoTask = {
+    id: Number,
+    content: String,
+    completed: Boolean,
+    timestamp: Number
+};
