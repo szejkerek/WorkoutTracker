d9952d5b857b3ee4a634d1cc0de36b5d20d09037
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-05-28 19:51:12 +0200

    Add post comment component

---
+++ src/lib/posts/PostCommentComp.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	export let commentData: PostComment;
+</script>
+
+<div class="flex flex-row justify-between w-full">
+	<div class="flex flex-row">
+		<div class="font-semibold text-black">
+			<button on:click={() => goto(`/user/${commentData.author.id}`)}>
+				{commentData.author.staticInfo.displayName}
+			</button>
+		</div>
+		<div class="text-black ml-2">
+			{commentData.content}
+		</div>
+	</div>
+	<div class="text-black">
+		{commentData.date}
+	</div>
+</div>
