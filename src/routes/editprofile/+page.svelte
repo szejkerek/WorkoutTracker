cf630fee1131a4e10fadfe9fa4848a9a332af92f
Marcin Mitręga <m.mitrega27@gmail.com>
2023-05-30 19:05:49 +0200

    Modifie positions

--- src/routes/editprofile/+page.svelte
+++ src/routes/editprofile/+page.svelte
@@ -30,30 +30,40 @@
 
 <div class="w-full h-full py-40 text-center bg-gray-300">
   <div class="flex flex-col justify-center items-center">
-    <label class="mb-4">
-      Username:
-      <input bind:value={user.username} type="text" placeholder="Username" class="input" on:input={() => changingField = 'username'} />
-    </label>
-    <label class="mb-4">
-      Age:
-      <input bind:value={user.staticInfo.age} type="number" placeholder="Age" class="input" on:input={() => changingField = 'age'} />
-    </label>
-    <label class="mb-4">
-      Gender:
-      <select bind:value={user.staticInfo.gender} class="input" on:change={() => changingField = 'gender'}>
-        {#each genderOptions as option}
-          <option value={option}>{option}</option>
-        {/each}
-      </select>
-    </label>
-    <label class="mb-4">
-      Display Name:
-      <input bind:value={user.staticInfo.displayName} type="text" placeholder="Display Name" class="input" on:input={() => changingField = 'display name'} />
-    </label>
-    <label class="mb-4">
-      Height (cm):
-      <input bind:value={user.staticInfo.heightInCM} type="number" placeholder="Height (cm)" class="input" on:input={() => changingField = 'height'} />
-    </label>
+    <div class="mb-4">
+      <label>
+        Username:
+        <input bind:value={user.username} type="text" placeholder="Username" class="input" on:input={() => changingField = 'username'} />
+      </label>
+    </div>
+    <div class="mb-4">
+      <label>
+        Age:
+        <input bind:value={user.staticInfo.age} type="number" placeholder="Age" class="input" on:input={() => changingField = 'age'} />
+      </label>
+    </div>
+    <div class="mb-4">
+      <label>
+        Gender:
+        <select bind:value={user.staticInfo.gender} class="input" on:change={() => changingField = 'gender'}>
+          {#each genderOptions as option}
+            <option value={option}>{option}</option>
+          {/each}
+        </select>
+      </label>
+    </div>
+    <div class="mb-4">
+      <label>
+        Display Name:
+        <input bind:value={user.staticInfo.displayName} type="text" placeholder="Display Name" class="input" on:input={() => changingField = 'display name'} />
+      </label>
+    </div>
+    <div class="mb-4">
+      <label>
+        Height (cm):
+        <input bind:value={user.staticInfo.heightInCM} type="number" placeholder="Height (cm)" class="input" on:input={() => changingField = 'height'} />
+      </label>
+    </div>
   </div>
 
   <div class="flex justify-center mt-6">
@@ -84,5 +94,6 @@
   }
   .input {
     @apply px-4 py-2 mt-2 rounded border-gray-400 border;
+    width: 100%; /* Dodajemy tę linijkę, aby elementy input miały tę samą szerokość */
   }
 </style>
