508a45a02cc054712ec1891311db5b2683eaf63a
Marcin Mitręga <m.mitrega27@gmail.com>
2023-05-30 20:08:51 +0200

    Add frame

--- src/routes/editprofile/+page.svelte
+++ src/routes/editprofile/+page.svelte
@@ -27,52 +27,56 @@
     goto("/");
   };
 </script>
+<div class="w-full h-full py-40 text-center bg-gray-300 flex justify-center items-center">
+  <div class="frame w-1/4">
+    <div class="flex flex-col justify-center items-center">
+      <p class="text-black font-black text-4xl mb-8">
+        Edit profile
+      </p>
+      <div class="mb-4">
+        <label>
+          <p class="text-black">Display Name:</p>
+          <input bind:value={user.staticInfo.displayName} type="text" placeholder="Display Name" class="input" on:input={() => changingField = 'display name'} />
+        </label>
+      </div>
+      <div class="mb-4">
+        <label>
+          <p class="text-black">Username:</p>
+          <input bind:value={user.username} type="text" placeholder="Username" class="input" on:input={() => changingField = 'username'} />
+        </label>
+      </div>   
+      <div class="mb-4">
+        <label>
+          <p class="text-black">Age:</p>
+          <input bind:value={user.staticInfo.age} type="number" placeholder="Age" class="input" on:input={() => changingField = 'age'} />
+        </label>
+      </div>
+      <div class="mb-4">
+        <label>
+          <p class="text-black">Height (cm):</p>
+          <input bind:value={user.staticInfo.heightInCM} type="number" placeholder="Height (cm)" class="input" on:input={() => changingField = 'height'} />
+        </label>
+      </div>
+      <div class="mb-4">
+        <label>
+          <p class="text-black">Gender:</p>
+          <select bind:value={user.staticInfo.gender} class="input" on:change={() => changingField = 'gender'}>
+            {#each genderOptions as option}
+              <option value={option}>{option}</option>
+            {/each}
+          </select>
+        </label>
+      </div>
+    </div>
 
-<div class="w-full h-full py-40 text-center bg-gray-300">
-  <div class="flex flex-col justify-center items-center">
-    <div class="mb-4">
-      <label>
-        Username:
-        <input bind:value={user.username} type="text" placeholder="Username" class="input" on:input={() => changingField = 'username'} />
-      </label>
+    <div class="flex justify-center mt-6">
+      <button on:click={saveProfile} class="btn btn-blue">
+        Save
+      </button>
+      <button on:click={cancelEditing} class="btn btn-secondary ml-4">
+        Cancel
+      </button>
     </div>
-    <div class="mb-4">
-      <label>
-        Age:
-        <input bind:value={user.staticInfo.age} type="number" placeholder="Age" class="input" on:input={() => changingField = 'age'} />
-      </label>
-    </div>
-    <div class="mb-4">
-      <label>
-        Gender:
-        <select bind:value={user.staticInfo.gender} class="input" on:change={() => changingField = 'gender'}>
-          {#each genderOptions as option}
-            <option value={option}>{option}</option>
-          {/each}
-        </select>
-      </label>
-    </div>
-    <div class="mb-4">
-      <label>
-        Display Name:
-        <input bind:value={user.staticInfo.displayName} type="text" placeholder="Display Name" class="input" on:input={() => changingField = 'display name'} />
-      </label>
-    </div>
-    <div class="mb-4">
-      <label>
-        Height (cm):
-        <input bind:value={user.staticInfo.heightInCM} type="number" placeholder="Height (cm)" class="input" on:input={() => changingField = 'height'} />
-      </label>
-    </div>
-  </div>
-
-  <div class="flex justify-center mt-6">
-    <button on:click={saveProfile} class="btn btn-blue">
-      Save
-    </button>
-    <button on:click={cancelEditing} class="btn btn-secondary ml-4">
-      Cancel
-    </button>
   </div>
 </div>
 
@@ -94,7 +98,15 @@
 
   .input {
     @apply px-4 py-2 mt-2 rounded-md border-gray-400  shadow-lg;
+    background-color:#391e46;
     width: 100%; /* Dodajemy tę linijkę, aby elementy input miały tę samą szerokość */
     
   }
+
+  .frame {
+    background-color: #c6c0c8;
+    padding: 20px;
+    border-radius: 10px; /*rafał*/
+  }
+
 </style>
