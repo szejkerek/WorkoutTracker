b7874bc579e662e7a47d6f0e97036a0ce7462023
Marcin Mitręga <m.mitrega27@gmail.com>
2023-05-30 11:41:44 +0200

    Add edit user profile page.
    Need to arrange postions, colors etc. Made on static user.

---
+++ src/routes/editprofile/+page.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { onMount } from "svelte";
+
+  let user: User = {
+    username: 'nnnnikodem',
+    password: '123123123',
+    email: 'whatwhatwhat@wp.xd',
+    staticInfo: {
+      age: 5,
+      avatarPath: '',
+      gender: 'Male',
+      displayName: 'niko',
+      heightInCM: 166,
+    }
+  };
+
+  let changingField: string;
+  let genderOptions: string[] = ['Male', 'Female', 'Other'];
+
+  function saveProfile() {
+    console.log("Profile saved:", user.username, user.staticInfo.age, user.staticInfo.gender, user.staticInfo.displayName, user.staticInfo.heightInCM);
+  }
+
+  //back to user profile
+  function cancelEditing() {
+    goto("/");
+  };
+</script>
+
+<div class="w-full h-full py-40 text-center bg-gray-300">
+  <div class="flex flex-col justify-center items-center">
+    <label class="mb-4">
+      Username:
+      <input bind:value={user.username} type="text" placeholder="Username" class="input" on:input={() => changingField = 'username'} />
+    </label>
+    <label class="mb-4">
+      Age:
+      <input bind:value={user.staticInfo.age} type="number" placeholder="Age" class="input" on:input={() => changingField = 'age'} />
+    </label>
+    <label class="mb-4">
+      Gender:
+      <select bind:value={user.staticInfo.gender} class="input" on:change={() => changingField = 'gender'}>
+        {#each genderOptions as option}
+          <option value={option}>{option}</option>
+        {/each}
+      </select>
+    </label>
+    <label class="mb-4">
+      Display Name:
+      <input bind:value={user.staticInfo.displayName} type="text" placeholder="Display Name" class="input" on:input={() => changingField = 'display name'} />
+    </label>
+    <label class="mb-4">
+      Height (cm):
+      <input bind:value={user.staticInfo.heightInCM} type="number" placeholder="Height (cm)" class="input" on:input={() => changingField = 'height'} />
+    </label>
+  </div>
+
+  <div class="flex justify-center mt-6">
+    <button on:click={saveProfile} class="btn btn-blue">
+      Save
+    </button>
+    <button on:click={cancelEditing} class="btn btn-secondary ml-4">
+      Cancel
+    </button>
+  </div>
+</div>
+
+<style>
+  .btn {
+    @apply font-bold py-2 px-4 rounded;
+  }
+  .btn-blue {
+    @apply bg-blue-500 text-white;
+  }
+  .btn-blue:hover {
+    @apply bg-blue-700;
+  }
+  .btn-secondary {
+    @apply bg-gray-400 text-white;
+  }
+  .btn-secondary:hover {
+    @apply bg-gray-600;
+  }
+  .input {
+    @apply px-4 py-2 mt-2 rounded border-gray-400 border;
+  }
+</style>
