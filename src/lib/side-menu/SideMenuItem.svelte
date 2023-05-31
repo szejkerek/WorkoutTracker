0b629f6d20f086db2112e38d4fd28755e205ab65
MikolajGajos <mikus22500@gmail.com>
2023-05-31 16:59:35 +0200

    Add side menu

---
+++ src/lib/side-menu/SideMenuItem.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+
+    export let name: string;
+    export let route: string;
+    export let icon: string;
+
+    
+</script>
+
+<button on:click={() => goto(route)} class="text-xl text-pink-500 font-semibold my-5 flex flex-row items-center">
+    <img src={icon} alt="xd" class="mr-5 w-10 h-10">
+    {name}
+</button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,21 @@
 <script>
 	import '../app.css';
-	import Header from '$lib/header/Header.svelte';
 	import { page } from '$app/stores';
+	import SideMenu from '$lib/side-menu/SideMenu.svelte';
 </script>
 
-<div class="flex flex-col bg-gray-300">
+<div class="flex flex-row bg-gray-300 max-h-screen">
 	{#if $page.route.id !== '/login' && $page.route.id !== '/register'}
-		<Header />
+	<div class="w-1/6 border-r border-eminence flex flex-row justify-center items-center my-10 box-border">
+		<SideMenu />		
+	</div>
 	{/if}
 
-	<slot />
+	<div class="w-2/3 overflow-y-scroll scrollbar-hide">
+		<slot />		
+	</div>
+	{#if $page.route.id !== '/login' && $page.route.id !== '/register'}
+	<div class="w-1/6 border-l border-eminence flex flex-row justify-center items-center my-10 box-border">
+	</div>
+	{/if}
 </div>
