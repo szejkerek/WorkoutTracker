773677bb0d5dd655f5d51a8b12b761b618ff88d2
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-06-03 23:40:50 +0200

    Refactor side menu rendering condition check

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,32 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 	import { page } from '$app/stores';
 	import SideMenu from '$lib/side-menu/SideMenu.svelte';
+
+	const routesWithNoSidemenu: (string | null)[] = [
+		'/login',
+		'/register',
+		'/start',
+		'/',
+		null
+	];
 </script>
 
 <div class="flex flex-row bg-gray-300 max-h-screen justify-center">
-	{#if $page.route.id !== '/login' && $page.route.id !== '/register' && $page.route.id !== '/start'}
-	<div class="w-1/6 border-r border-wisteria flex flex-row justify-center items-center my-10 box-border">
-		<SideMenu />		
-	</div>
+	{#if !routesWithNoSidemenu.includes($page.route.id)}
+		<div
+			class="w-1/6 border-r border-wisteria flex flex-row justify-center items-center my-10 box-border"
+		>
+			<SideMenu />
+		</div>
 	{/if}
 
 	<div class="w-2/3 overflow-y-scroll scrollbar-hide">
-		<slot />		
+		<slot />
 	</div>
-	{#if $page.route.id !== '/login' && $page.route.id !== '/register' && $page.route.id !== '/start'}
-	<div class="w-1/6 border-l border-wisteria flex flex-row justify-center items-center my-10 box-border">
-	</div>
+	{#if !routesWithNoSidemenu.includes($page.route.id)}
+		<div
+			class="w-1/6 border-l border-wisteria flex flex-row justify-center items-center my-10 box-border"
+		/>
 	{/if}
 </div>
