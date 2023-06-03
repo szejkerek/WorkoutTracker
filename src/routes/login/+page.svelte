7e42909785ddf77d5c9bca39ad735158ffb3c915
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-06-03 23:40:14 +0200

    Check if credentials are ok and start session

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,88 +1,90 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import Logo from '$lib/static/Logo.png';
+	import { userSessionData } from '../../stores/userSession';
+
 	let username: String;
 	let password: String;
 
-	let currUser: User;
+	let incorrectUsername = false;
+	let incorrectPassword = false;
 
 	const login = async () => {
-		const categoriesXD = await fetch(
-			`/api/exercise_category?username=${username}`
-		);
-		const response = await categoriesXD.json();
+		const resp = await fetch('/api/users');
+		const users: User[] = (await resp.json()).data;
 
-		if (response.code !== 1) {
-			console.log('xd');
-			console.log(response);
+		const foundUser = users.find(
+			(user) => user.username.toLowerCase() === username.toLowerCase()
+		);
+
+		if (foundUser === undefined) {
+			incorrectUsername = true;
 
 			return;
 		}
 
-		console.table(response);
+		incorrectUsername = false;
+
+		if (foundUser.password !== password) {
+			incorrectPassword = true;
+
+			return;
+		}
+
+		incorrectPassword = false;
+		$userSessionData = foundUser;
+		goto('/feed');
 	};
-
-	// export const login = async () => {
-	// 	const userInfo = await fetch(`/api/user_info/?username=${username}`);
-
-	// 	let response = await userInfo.json();
-
-	// 	if (response.code !== 1) {
-	// 		console.log('xd');
-
-	// 		return;
-	// 	}
-
-	// 	currUser = response.data;
-	// };
-
-	// const login = async () => {
-	// 	const response = await fetch('/api/add_user', {
-	// 		method: 'POST',
-	// 		body: JSON.stringify({ password, username }),
-	// 		headers: {
-	// 			'content-type': 'application/json'
-	// 		}
-	// 	});
-	// };
-
-	import Logo from '$lib/static/Logo.png'
 </script>
 
-<div class="text-red-500">
-	{#if currUser !== undefined}
-		<!-- <p>{currUser.username}</p>
-		<p>{currUser.email}</p>
-		<p>{currUser.password}</p>
-		<p>{currUser.info.gender}</p>
-		<p>{currUser.bodyProfile.dateOfMeasurement}</p> -->
-	{/if}
-</div>
-<div class='flex flex-col items-center min-h-screen p-10 w-full mx-auto'>
-    <div class="pt-16">
-		<img src={Logo} alt="cipa" width=350px height=350px>
+<div class="flex flex-col items-center min-h-screen p-10 w-full mx-auto">
+	<div class="pt-16">
+		<img src={Logo} alt="cipa" width="350px" height="350px" />
 	</div>
-	<br>
+	<br />
 	<form class="w-full max-w-sm flex flex-col justify-center">
 		<div class="flex justify-center mb-6">
-		<div>
-			<input class="border-2 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white placeholder-gray-900" 
-			id="inline-full-name" 
-			type="text" 
-			placeholder="Username" 
-			style="background-color:#D8B4E2; border-color:#55286F; color:black; width:300px; height:45px">
-		</div>
-		</div>
-		<div class="flex justify-center mb-6">
-		<div>
-			<input class="border-2 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white placeholder-gray-900" 
-			id="inline-password" 
-			type="password" 
-			placeholder="Password"
-			style="background-color:#D8B4E2; border-color:#55286F; color:black; width:300px; height:45px">
-		</div>
+			<div>
+				<input
+					class="border-2 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white placeholder-gray-900"
+					bind:value={username}
+					id="inline-full-name"
+					type="text"
+					placeholder="Username"
+					style="background-color:#D8B4E2; border-color:#55286F; color:black; width:300px; height:45px"
+				/>
+			</div>
 		</div>
 		<div class="flex justify-center mb-6">
-		<div>
-			<button class='btn w-40 btn-accent mt-4'>Login</button>
+			<div>
+				<input
+					class="border-2 rounded py-2 px-4 leading-tight focus:outline-none focus:bg-white placeholder-gray-900"
+					bind:value={password}
+					id="inline-password"
+					type="password"
+					placeholder="Password"
+					style="background-color:#D8B4E2; border-color:#55286F; color:black; width:300px; height:45px"
+				/>
+			</div>
+		</div>
+		<div class="flex justify-center mb-6">
+			<div>
+				<button
+					on:click={() => login()}
+					class="btn w-40 btn-accent mt-4">Login</button
+				>
+			</div>
+		</div>
+		<div class="flex justify-center">
+			{#if incorrectUsername}
+				<p class="text-red-500 font-semibold">
+					No such username exists.
+				</p>
+			{/if}
+
+			{#if incorrectPassword}
+				<p class="text-red-500 font-semibold">Incorrect password.</p>
+			{/if}
 		</div>
 	</form>
 </div>
