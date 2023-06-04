71dbef7981334e36c2d193bac13f7670af1d5512
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-06-04 15:08:21 +0200

    Create directory for enum definitions

---
+++ src/lib/enums/errorCodes.ts
@@ -0,0 +1,8 @@
+export enum ResultCodes {
+    SUCCESS = 1,
+    ERROR = 1 << 8,
+    FETCH_ERROR = ERROR + (1 << 1),
+    RECORD_EXISTS = ERROR + (1 << 2),
+    RECORD_DOES_NOT_EXIST = ERROR + (1 << 3),
+    INVALID_REQUEST_PARAMS = ERROR + (1 << 4),
+}
