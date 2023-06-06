455709a6423439c4b0acd96060298fed7dc6a003
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-06-06 19:15:26 +0200

    Add 2nd sidebar

--- src/lib/side-menu/SideMenu.svelte
+++ src/lib/side-menu/SideMenu.svelte
@@ -1,53 +1,13 @@
 <script lang="ts">
 	import SideMenuItem from './SideMenuItem.svelte';
-	import homeIcon from '$lib/static/icons/home-icon.svg';
-	import searchIcon from '$lib/static/icons/search-icon.svg';
-	import notificationsIcon from '$lib/static/icons/notifications-icon.svg';
-	import profileIcon from '$lib/static/icons/profile-icon.svg';
-	import settingsIcon from '$lib/static/icons/settings-icon.svg';
-	import reportIcon from '$lib/static/icons/report-icon.svg';
 	import logo from '$lib/static/Logo.png';
-	import { userSessionData } from '../../stores/userSession';
+	import homeIcon from '$lib/static/icons/home-icon.svg';
 
-	let pages: {
+	export let pages: {
 		name: string;
 		route: string;
 		icon: string;
-	}[] = [
-		{
-			icon: homeIcon,
-			name: 'Home',
-			route: '/feed'
-		},
-		{
-			icon: searchIcon,
-			name: 'Search',
-			route: '/feed'
-		},
-		{
-			icon: notificationsIcon,
-			name: 'Notifications',
-			route: '/feed'
-		},
-		{
-			icon: profileIcon,
-			name: 'Profile',
-			route:
-				$userSessionData === null
-					? '/start'
-					: `/user/${$userSessionData.id}`
-		},
-		{
-			icon: settingsIcon,
-			name: 'Settings',
-			route: '/feed'
-		},
-		{
-			icon: reportIcon,
-			name: 'Report',
-			route: '/feed'
-		}
-	];
+	}[];
 </script>
 
 <div class="w-1/2 flex flex-col items-start justify-between h-screen pb-16">
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,13 @@
 	import '../app.css';
 	import { page } from '$app/stores';
 	import SideMenu from '$lib/side-menu/SideMenu.svelte';
+	import homeIcon from '$lib/static/icons/home-icon.svg';
+	import searchIcon from '$lib/static/icons/search-icon.svg';
+	import notificationsIcon from '$lib/static/icons/notifications-icon.svg';
+	import profileIcon from '$lib/static/icons/profile-icon.svg';
+	import settingsIcon from '$lib/static/icons/settings-icon.svg';
+	import reportIcon from '$lib/static/icons/report-icon.svg';
+	import { userSessionData } from '../stores/userSession';
 
 	const routesWithNoSidemenu: (string | null)[] = [
 		'/login',
@@ -10,6 +17,86 @@
 		'/',
 		null
 	];
+
+	let leftPages: {
+		name: string;
+		route: string;
+		icon: string;
+	}[] = [
+		{
+			icon: homeIcon,
+			name: 'Home',
+			route: '/feed'
+		},
+		{
+			icon: searchIcon,
+			name: 'Search',
+			route: '/feed'
+		},
+		{
+			icon: notificationsIcon,
+			name: 'Notifications',
+			route: '/feed'
+		},
+		{
+			icon: profileIcon,
+			name: 'Profile',
+			route:
+				$userSessionData === null
+					? '/start'
+					: `/user/${$userSessionData.id}`
+		},
+		{
+			icon: settingsIcon,
+			name: 'Settings',
+			route: '/feed'
+		},
+		{
+			icon: reportIcon,
+			name: 'Report',
+			route: '/feed'
+		}
+	];
+
+	let rightPages: {
+		name: string;
+		route: string;
+		icon: string;
+	}[] = [
+		{
+			icon: homeIcon,
+			name: 'Home',
+			route: '/feed'
+		},
+		{
+			icon: searchIcon,
+			name: 'Search',
+			route: '/feed'
+		},
+		{
+			icon: notificationsIcon,
+			name: 'Notifications',
+			route: '/feed'
+		},
+		{
+			icon: profileIcon,
+			name: 'Profile',
+			route:
+				$userSessionData === null
+					? '/start'
+					: `/user/${$userSessionData.id}`
+		},
+		{
+			icon: settingsIcon,
+			name: 'Settings',
+			route: '/feed'
+		},
+		{
+			icon: reportIcon,
+			name: 'Report',
+			route: '/feed'
+		}
+	];
 </script>
 
 <div class="flex flex-row bg-gray-300 max-h-screen justify-center">
@@ -17,7 +104,7 @@
 		<div
 			class="w-1/6 border-r border-wisteria flex flex-row justify-center items-center my-10 box-border"
 		>
-			<SideMenu />
+			<SideMenu pages={leftPages} />
 		</div>
 	{/if}
 
@@ -27,6 +114,8 @@
 	{#if !routesWithNoSidemenu.includes($page.route.id)}
 		<div
 			class="w-1/6 border-l border-wisteria flex flex-row justify-center items-center my-10 box-border"
-		/>
+		>
+			<SideMenu pages={rightPages} />
+		</div>
 	{/if}
 </div>
