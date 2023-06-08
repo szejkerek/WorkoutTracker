6132e061396f734eef203b0fa6ecb312864e9e79
lilboisoul <kdusza2001@gmail.com>
2023-06-08 11:43:05 +0200

    Start workout main rework

--- src/routes/workout-main/+page.svelte
+++ src/routes/workout-main/+page.svelte
@@ -1,62 +1,38 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
     import { onMount } from "svelte";
-    import icon_calendar from "$lib/static/icons/icon-calendar.png";
-    import icon_plus from "$lib/static/icons/icon-plus.png";
+    import icon_calendar from "$lib/static/icons/calendar-icon.svg";
+    import icon_workout from "$lib/static/icons/workout-icon.svg";
+    import { userSessionData } from '../../stores/userSession.js';
   
-    let user: User = {
-      username: 'nnnnikodem',
-      password: '123123123',
-      email: 'whatwhatwhat@wp.xd',
-      staticInfo: {
-        age: 5,
-        avatarPath: '',
-        gender: 'Male',
-        displayName: 'niko',
-        heightInCM: 166,
-      }
-    }
+    let ready = false;
+  
+	onMount(() => {
+		if ($userSessionData === null) {
+			goto('/start');
+		} else {
+			ready = true;
+		}
+	});
 
-    let sampleExercise: Exercise = {
-        id: "DyoXbrSe7ghNxon5BbUk",
-        category: {
-            id: "14JYkBMcU7djAh0SNKd7",
-            name: "Łapy",
-            note: "bicek",
-            owner: user,
-        },
+    let todayDate = new Date();
+    let day = todayDate.getDate() < 10 ? "0" + todayDate.getDate() : todayDate.getDate();
+    let month = todayDate.getMonth() + 1  < 10 ? "0" + (todayDate.getMonth() + 1) : todayDate.getMonth() + 1;
+    let parsedDate = day + "." + month + "." + todayDate.getFullYear();
 
-        displayName: "cwiczenie na bica",
-        exerciseType: 1,
-        note: "notka"
-    }
-    let previousWorkout: DoneExercise[] = [
-
-    ];
-
-    let doneExercises: DoneExercise = {
-        id: "id",
-        owner: user,
-        exercise: sampleExercise,
-        date: "31-05-2023",
-        distanceInMeters: 0,
-        timeInSeconds: 0,
-        weightInKG: 15,
-        repetitions: 15
-    }
   
     function startNewWorkout() {
         goto("/workout-categories");
     }
 
     function copyPreviousWorkout() {
-        goto("/workout-calendar");
+        goto("/calendar");
     };
   </script>
-
+{#if ready}
   <div class="w-full h-full py-10 text-center bg-gray-300 flex flex-col justify-center items-center">
    <div class="text-black font-bold text-3xl mb-10">
-    <h1>Hello, {user.username}!</h1>
+    <h1>Hello, {$userSessionData?.staticInfo.displayName}!</h1>
    </div>
 
    <div class="bg-eminence rounded-md p-4 w-60 text-white">
@@ -69,28 +45,27 @@
    </div>
 
    <div class="flex justify-around items-center flex-row w-96 bg-wisteria my-10 rounded-md text-black font-semibold">
-    <button>previous</button>
-    <p>31.05.2023</p>
+    <button>prev</button>
+    <p>{parsedDate}</p>
     <button>next</button>
     </div>
 
    <div class="flex flex-row items-center">
         <div class="flex flex-col justify-center mt-6 px-10 w-2/3">
             <div class="flex flex-row items-center w-full mb-5">
-                <img src={icon_plus} alt="Start new workout plus icon" class="w-10 h-10 mr-5">
-                <button on:click={startNewWorkout} class="butn hover:bg-gray-400">
+                <img src={icon_workout} alt="Start new workout icon" class="w-10 h-10 mr-5">
+                <button on:click={startNewWorkout} class="font-semibold py-2 px-4 rounded bg-gray-300 text-black text-lg hover:bg-gray-400">
                 Start new workout
                 </button>
             </div>
             <div class="flex flex-row items-center w-full">
-                <img src={icon_calendar} alt="Copy previous workout calendar icon" class="w-10 h-10 mr-5">
-                <button on:click={copyPreviousWorkout} class="butn hover:bg-gray-400">
+                <img src={icon_calendar} alt="Copy previous workout icon" class="w-10 h-10 mr-5">
+                <button on:click={copyPreviousWorkout} class="font-semibold py-2 px-4 rounded bg-gray-300 text-black text-lg hover:bg-gray-400">
                 Copy previous workout
                 </button>
             </div>
         </div> 
 
-    {#if previousWorkout.length == 0}
     <div class="flex flex-col justify-center text-black mt-10 px-10 font-bold text-3xl">
         <h2 class="mb-2">Previous workout</h2>
         <div class="rounded-md bg-wisteria font-light text-base py-2 m-2">
@@ -118,15 +93,10 @@
             </ul>
         </div>
    </div>
-   {/if}
   </div>
 </div>
-  
+{/if}
   <style>
-    .butn {
-      @apply font-semibold py-2 px-4 rounded bg-gray-300;
-      @apply text-black text-lg;
-    }
     ul {
       @apply list-none;
     }
