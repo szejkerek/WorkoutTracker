9281cdf200a98005da559ad909a4336afdb4aea3
MikolajGajos <mikus22500@gmail.com>
2023-06-08 14:37:52 +0200

    Add following communicate to search bar results

--- src/lib/profile-result/ProfileResult.svelte
+++ src/lib/profile-result/ProfileResult.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
 	import { goto } from "$app/navigation";
+	import { onMount } from "svelte";
+	import { userSessionData } from "../../stores/userSession";
     export let user: User;
     import profileDefault from "../static/ProfileDefault.svg";
+
+    let followedByUser: boolean = false;
+    let followedByFoundUser: boolean = false;
+    
+    onMount(() => {
+        if($userSessionData === null)
+            return;
+        followedByUser = $userSessionData?.followingIds.includes(user.id);
+        followedByFoundUser = user.followingIds.includes($userSessionData.id);
+    })
 </script>
 
 <div class="py-3 flex flex-row border-b border-wisteria mb-5">
@@ -14,6 +26,15 @@
             </span>
             <span class="font-semibold text-black">
                 @{user.username}
+            </span>
+            <span class="font-semibold text-eminence">
+                {#if followedByUser && followedByFoundUser}           
+                    You follow each other
+                {:else if followedByUser}
+                    Following
+                {:else if followedByFoundUser}
+                    Follows you
+                {/if}
             </span>
         </div>
     </button>  
