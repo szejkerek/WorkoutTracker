89ebbbddf12ae689f6d0db3181d18317e67f85d1
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-06-13 22:24:56 +0200

    Add all variations

---
+++ src/lib/enums/exerTypes.ts
@@ -0,0 +1,12 @@
+export enum ExerciseEnum {
+    Weight = 1,
+    Reps = 1 << 1,
+    Distance = 1 << 2,
+    Time = 1 << 3,
+    WeightReps = Weight + Reps,
+    WeightDistance = Weight + Distance,
+    WeightTime = Time + Weight,
+    TimeReps = Time + Reps,
+    DistanceTime = Distance + Time,
+    RepsDistance = Reps + Distance
+}
