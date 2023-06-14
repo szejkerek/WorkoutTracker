d65b0f946ad0426d0fb73fcef069e289662b86d5
Paweł Kupczak <pawel.kupczak.sc@gmail.com>
2023-06-14 19:40:36 +0200

    Update workout view according to set date and mutate it

--- src/routes/workout/main/+page.svelte
+++ src/routes/workout/main/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
-	import icon_calendar from '$lib/static/icons/calendar-icon.svg';
-	import icon_workout from '$lib/static/icons/workout-icon.svg';
 	import { userSessionData } from '../../../stores/userSession.js';
 	import arrow_left from '$lib/static/icons/arrow-left.svg';
 	import arrow_right from '$lib/static/icons/arrow-right.svg';
@@ -17,13 +15,21 @@
 		if ($userSessionData === null) {
 			goto('/start');
 		} else {
+			currentDate = new Date();
+			parseDate();
+
 			const response = await fetch('/api/doneExercises');
 			const data: DoneExercise[] = (await response.json()).data;
 
 			$detsStore = data;
+			updateDayExercises();
 			ready = true;
 		}
 	});
+
+	const updateDayExercises = () => {
+		currentExercises = $detsStore.filter((ex) => ex.date == parsedDate);
+	};
 
 	function parseDate() {
 		day = currentDate.getDate();
@@ -37,11 +43,13 @@
 	function decrementDay() {
 		currentDate.setDate(currentDate.getDate() - 1);
 		parseDate();
+		updateDayExercises();
 	}
 
 	function incrementDay() {
 		currentDate.setDate(currentDate.getDate() + 1);
 		parseDate();
+		updateDayExercises();
 	}
 
 	function startNewWorkout() {
@@ -52,32 +60,29 @@
 		goto('/calendar');
 	}
 
-	export let data;
-
-	let todayDate = new Date();
-	let currentDate = todayDate;
+	let currentExercises: DoneExercise[] = [];
+	let currentDate: Date = new Date();
 	let day;
 	let month;
 	let parsedDate: String;
 	parseDate();
-
-	//to-do
-	//get done exercises with date = currentDate and user = currently logged in user
-	//if any exercises with date = currentDate exist => display them below the date-thingy
-	//else display previous workout and buttons enabling the user to start a new workout or to copy a previously done workout
 </script>
 
 {#if ready}
 	<div
-		class="w-full h-full py-10 text-center bg-gray-300 flex flex-col justify-center items-center"
+		class="w-full max-h-screen pt-10 text-center bg-gray-300 flex flex-col justify-center items-center"
 	>
-		<div class="w-full h-1/3 flex flex-col justify-center items-center">
+		<div
+			class="w-full h-1/3 flex flex-col justify-center items-center shadow-xl border-b border-wisteria"
+		>
 			<div class="text-black font-bold text-3xl mb-10">
 				<h1>Hello, {$userSessionData?.staticInfo.displayName}!</h1>
 			</div>
 
 			<div class="bg-eminence rounded-md p-4 w-60 text-white">
-				<p class="font-normal text-xl italic mb-2">"motivational quote"</p>
+				<p class="font-normal text-xl italic mb-2">
+					"motivational quote"
+				</p>
 				<p class="font-bold text-2xl">~gym bro</p>
 			</div>
 
@@ -85,40 +90,46 @@
 				class="flex justify-around items-center flex-row w-96 bg-wisteria my-10 rounded-md text-black font-semibold py-2"
 			>
 				<button on:click={decrementDay}>
-					<img src={arrow_left} alt="Previous day icon" class="w-5 h-5" />
+					<img
+						src={arrow_left}
+						alt="Previous day icon"
+						class="w-5 h-5"
+					/>
 				</button>
 				<p class="text-xl">{parsedDate}</p>
 				<button on:click={incrementDay}>
-					<img src={arrow_right} alt="Next day icon" class="w-5 h-5" />
+					<img
+						src={arrow_right}
+						alt="Next day icon"
+						class="w-5 h-5"
+					/>
 				</button>
 			</div>
 			<h2 class="mb-6 font-bold text-3xl text-black">Current workout</h2>
 
-			<div class="flex flex-row justify-between mb-16 items-center">
-					<img
+			<div class="flex flex-row justify-between mb-5 items-center">
+				<img
 					src={plusIcon}
 					alt="Start new workout icon"
 					class="w-10 h-10 mr-5"
-					/>
-					<button
-						on:click={() => goto('/workout/categories')}
-						class="font-semibold py-2 px-4 rounded bg-gray-300 text-black text-xl hover:bg-gray-400"
-					>
-						Add new exercise
-					</button>
+				/>
+				<button
+					on:click={() => goto('/workout/categories')}
+					class="font-semibold py-2 px-4 rounded bg-gray-300 text-black text-xl hover:bg-gray-400"
+				>
+					Add new exercise
+				</button>
 			</div>
-
 		</div>
-		
-			
-				<div class="flex flex-row w-full justify-center mt-10 px-10 flex-wrap overflow-y-scroll scrollbar-hide h-fit border-t border-wisteria ">
-					
-					{#each $detsStore as ex}
-						<div class="w-1/3 px-10 py-5 ">
-							<DoneExerciseComp xd={ex} />
-						</div>
-					{/each}
+
+		<div
+			class="flex flex-row w-full justify-center flex-wrap overflow-y-scroll scrollbar-hide"
+		>
+			{#each currentExercises as ex}
+				<div class="xl:w-1/3 lg:w-1/2 w-full px-10 py-5">
+					<DoneExerciseComp xd={ex} />
 				</div>
-			
+			{/each}
+		</div>
 	</div>
 {/if}
